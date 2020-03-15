csci312a-s18/fp-middrides

Initial commit with client and server skeleton

server/index.js
@@ -1,0 +1,5 @@
+const http = require('http');
+const { app } = require('./server');
+
+const server = http.createServer(app).listen(process.env.PORT || 3001);
+console.log('Listening on port %d', server.address().port); // eslint-disable-line no-console

csci312a-s20/project-skeleton

server/index.js
@@ -1,0 +1,5 @@
+const http = require('http');
+const { app } = require('./routes');
+
+const server = http.createServer(app).listen(process.env.PORT || 3001);
+console.log('Listening on port %d', server.address().port); // eslint-disable-line no-console